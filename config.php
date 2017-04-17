f78a51134203dc2cfe7d9e6c86f63b1f98f35b0d
Salumão Barbosa da Costa <salubcosta@gmail.com>
2017-04-17 02:21:43 -0300

    arquivo de configuracao - autoload e constantes para conectar com db

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'environment.php';
+
+if(ENVIRONMENT == 'development'){
+	//INFORMAÇÕES DO AMBIENTE DE DESENVOLVIMENTO
+	define('HOST','localhost');
+	define('DBNAME','db_twitter');
+	define('DBUSER','root');
+	define('DBPASS','root');
+}else{
+	//INFORMAÇÕES DO AMBIENTE DE PRODUÇÃO
+	define('HOST',''); 
+	define('DBNAME','');
+	define('DBUSER','');
+	define('DBPASS','');
+}
+
+spl_autoload_register(function($class){
+	if(file_exists(DIRETORIO.'/controllers/'.$class.'.php')){
+		require_once DIRETORIO.'/controllers/'.$class.'.php';
+	}elseif(file_exists(DIRETORIO.'/models/'.$class.'.php')){
+		require_once DIRETORIO.'/models/'.$class.'.php';
+	}else{
+		require_once DIRETORIO.'/core/'.$class.'.php';
+	}
+});
