57089c4732b8184ce31e2a489e39173c4c25375e
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-04-18 18:47:46 -0300

    improve home view

--- views/home.php
+++ views/home.php
@@ -1,5 +1,26 @@
-Área Interna do Twitter-TI.
+<div class="feed">
+	Feeds...
+</div>
 
-<p>
-<a href="<?php echo URL?>/login/sair">Sair</a>
-</p>
+<div class="rightside">
+	<h4>Relacionamentos</h4>
+	<div class="rs_meio"><?php echo $qtdSeguidores;?> <br />Seguidores</div>
+	<div class="rs_meio"><?php echo $qtdSeguidos;?> <br />Seguindo</div>
+	<div style="clear: both;"></div>
+
+	<h4>Sugestão</h4>
+	<table border="0" width="100%">
+		<tr>
+			<td width="80%"></td>
+			<td></td>
+		</tr>
+		<?php foreach($sugestao as $usuario): ?>
+		<tr>
+			<td><?php echo $usuario['NOME'];?></td>
+			<td>
+				<a href="#">Seguir</a>
+			</td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+</div>
