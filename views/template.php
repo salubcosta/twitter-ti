2e5251eff8f5a1e026596faff2cbd8316e5a26d2
Salumao Barbosa da Costa <salubcosta@gmail.com>
2017-04-18 18:48:50 -0300

    improve template view

--- views/template.php
+++ views/template.php
@@ -2,8 +2,20 @@
 <html lang="pt-br">
 <head>
 	<title>Twitter - TI</title>
+	<link rel="stylesheet" href="<?php echo URL;?>/assets/css/template.css" />
+	<script type="text/javascript" src="<?php echo URL;?>/assets/js/jquery-3.0.0.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL;?>/assets/js/script.js"></script>
 </head>
 <body>
-<?php $this->carregarViewNoTemplate($view,$array); ?>
+	<div class="topo">
+		<div class="topoint">
+			<div class="topoleft">TWITTER-TI</div>
+			<div class="toporight"><?php echo $array['nome']; ?> - <a href="<?php echo URL;?>/login/sair">Sair</a></div>
+			<div style="clear: both;"></div>
+		</div>
+	</div>
+	<div class="container">
+		<?php $this->carregarViewNoTemplate($view,$array); ?>
+	</div>
 </body>
 </html>
